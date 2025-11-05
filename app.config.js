300aa747dbabb9e3427d21155262c8cb9cb9934d
blessing_u <ubiomorblessing@gmail.com>
2025-11-05 09:50:29 +0100

    chore: add the right image path.

--- app.config.js
+++ app.config.js
@@ -7,25 +7,32 @@ export default {
     icon: "./assets/icon.png",
     userInterfaceStyle: "light",
     splash: {
-      image: "./assets/splash.png",
+      image: "./assets/splash-icon.png",
       resizeMode: "contain",
       backgroundColor: "#ffffff",
     },
     assetBundlePatterns: ["**/*"],
     ios: {
       supportsTablet: true,
+      bundleIdentifier: "com.mzb.audiophile",
     },
     android: {
       adaptiveIcon: {
         foregroundImage: "./assets/adaptive-icon.png",
         backgroundColor: "#ffffff",
       },
+      package: "com.mzb.audiophile",
     },
     web: {
       favicon: "./assets/favicon.png",
     },
     extra: {
-      convexUrl: process.env.EXPO_PUBLIC_CONVEX_URL || "http://127.0.0.1:6790",
+      convexUrl:
+        process.env.EXPO_PUBLIC_CONVEX_URL ||
+        "https://little-butterfly-666.convex.cloud",
+      eas: {
+        projectId: "ad002c86-1312-4539-ba45-1455814d1839", // Add this line
+      },
     },
   },
 };
